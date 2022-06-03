d973fcbb5eb4ad212a46c72077e847505dcda1f5
Rosalio Monterrosa <rosalioalfre2@gmail.com>
2022-06-03 08:33:40 -0600

    date picker

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,11 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+
 </head>
 <body>
 
@@ -74,4 +79,14 @@
     @yield('content')
     </main>
 </body>
+    <script type="text/javascript">
+        $('.date').datepicker({  
+        format: 'mm-dd-yyyy'
+        });  
+    </script>
+
+    <!--Date picker-->
+    <!--
+        <input class="date form-control" type="text" readonly="readonly">
+    -->
 </html>
