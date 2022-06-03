26dc9a0bc33112c04916ae5cae9dc754f185e684
KatyaCarbajal <89698426+KatyaCarbajal@users.noreply.github.com>
2022-06-03 09:13:16 -0600

    Arreglando la vista gestionar cirugía
    Arreglando botones

--- resources/views/Cirugia/GestionarCirugia.blade.php
+++ resources/views/Cirugia/GestionarCirugia.blade.php
@@ -15,11 +15,7 @@ Gestionar Cirugia
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
-        </div>
-        <div class="column">
-            <a href="/crear_cirugia"><button class="btn btn-success">Crear cita de cirugía</button></a>
-        </div>
-    </div>
+    
 </div>
 @endsection
 
