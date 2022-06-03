05a16305145210487abd4e3d1bd795cde3c8983a
Luis Molina <alfonsomduque@gmail.com>
2022-06-03 00:20:59 -0600

    tablas iniciales

---
+++ database/migrations/2022_06_03_053518_cita_vacuna.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CitaVacuna extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citaVacuna', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //convencion de laravel para llaves foraneas, no estoy seguro del cascade on delete o en oupdate
+            $table->foreignId('cita_id')->constrained('cita')->cascadeOnDelete();
+            $table->foreignId('vacuna_id')->constrained('vacuna')->cascadeOnDelete();
+            $table->dateTime('fechaAplicacion');
+            $table->dateTime('siguienteAplicacion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_06_03_055029_cita_servicio.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CitaServicio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citaServicio', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //convencion de laravel para llaves foraneas, no estoy seguro del cascade on delete o en oupdate
+            $table->foreignId('cita_id')->constrained('cita')->cascadeOnDelete();
+            $table->foreignId('tipoServicio_id')->constrained('tipoServicio')->cascadeOnDelete();
+            $table->string('notaServicio',30);
+            $table->string('nombreMascotaServicio',15);
+            $table->dateTime('fechaCitaServicio');
+            $table->string('clienteServicio',20);
+            $table->string('telefonoServicio',8);
+            $table->boolean('estadoServicio');
+            $table->time('horaServicio');
+            $table->string('razaServicio',8);
+            $table->string('colorServicio',8);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
