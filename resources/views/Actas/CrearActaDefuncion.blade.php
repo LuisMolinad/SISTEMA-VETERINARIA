75a151cfeb96d8a08afd966c9c0436d052e2df43
Luis Molina <alfonsomduque@gmail.com>
2022-06-04 01:13:42 -0600

    Formulario completo crear acta defuncion

--- resources/views/Actas/CrearActaDefuncion.blade.php
+++ resources/views/Actas/CrearActaDefuncion.blade.php
@@ -14,50 +14,35 @@ Actas de defunción
 @section('content')
 <div class="container">
     <form>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Email</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-          </div>
-          <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-          </div>
-        </div>
         <div class="form-group">
-          <label for="inputAddress">Address</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
+            <label for="IDMASCOTA">IDMASCOTA</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="IDMASCOTA">
+          </div>
+          <div class="form-group">
+            <label for="inputNombreMascota">Mascota</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Nombre Mascota">
+          </div>
+          <div class="form-row" style="background-color:#d3e9ff">
+            <div class="form-group col-md-6">
+              <label for="inputnombreDuenio">Dueño</label>
+              <input type="text" class="form-control" id="inputnombreDuenio" placeholder="Nombre del dueño">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputContactNumber">Número de contacto</label>
+              <input type="text" class="form-control" id="inputContactNumber" placeholder="Número de contacto">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputDireccion">Direccion</label>
+                <input type="text" class="form-control" id="inputDireccion" placeholder="Direccion del dueño">
+              </div>
+          </div>
         <div class="form-group">
-          <label for="inputAddress2">Address 2</label>
-          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            <label for="InputCausaFallecimiento">Causa del fallecimiento</label>
+            <input type="text" class="form-control" style="width: 550px; height: 120px;" id="InputCausaFallecimiento" placeholder="Causa del fallecimiento">
         </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputCity">City</label>
-            <input type="text" class="form-control" id="inputCity">
-          </div>
-          <div class="form-group col-md-4">
-            <label for="inputState">State</label>
-            <select id="inputState" class="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div class="form-group col-md-2">
-            <label for="inputZip">Zip</label>
-            <input type="text" class="form-control" id="inputZip">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+
+
+        <button type="submit"  class="btn btn-primary">Guardar</button>
       </form>
 </div>
 @endsection
