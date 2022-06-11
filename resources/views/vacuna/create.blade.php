8d1c9752cf6ba9bda8486f22b854372b83a2ea4e
abenabi <abenabigarcia@gmail.com>
2022-06-11 12:48:28 -0600

    andres solucion errores

--- resources/views/vacuna/create.blade.php
+++ resources/views/vacuna/create.blade.php
@@ -17,17 +17,17 @@ Nueva vacuna
             <legend class="legend">Vacuna</legend>
                 <div class="form-group">
                     <label for="nombreVacuna">Nombre de la vacuna</label>
-                    <input type="text" maxlength="20" class="form-control" id="nombreVacuna" name="nombreVacuna" placeholder="Ingrese el nombre de la vacuna">
+                    <input type="text" maxlength="20" class="form-control" id="nombreVacuna" name="nombreVacuna" placeholder="Ingrese el nombre de la vacuna" required>
                 </div>
 
                 <div class="form-group">
                     <label for="descripcionVacuna">Descripción de la vacuna</label>
-                    <input type="text" maxlength="50" class="form-control" id="descripcionVacuna" name="descripcionVacuna" placeholder="Ingrese una descripción de la vacuna">
+                    <input type="text" maxlength="50" class="form-control" id="descripcionVacuna" name="descripcionVacuna" placeholder="Ingrese una descripción de la vacuna" required>
                 </div>
 
                 <div class="form-group">
                     <label for="tiempoEntreDosisDia">Tiempo entre dósis</label>
-                    <input type="number" class="form-control" id="tiempoEntreDosisDia" name="tiempoEntreDosisDia" placeholder="Ingrese el tiempo entre dosis de la vacuna en días">
+                    <input type="number" class="form-control" id="tiempoEntreDosisDia" name="tiempoEntreDosisDia" placeholder="Ingrese el tiempo entre dosis de la vacuna en días" required>
                 </div>
             </fieldset>
             <button type="submit" class="btn btn-success mb-2">Crear</button>
