e9726fa40a187471143d87c8c2b3a0bf5d4677c0
Rosalio Monterrosa <rosalioalfre2@gmail.com>
2022-08-09 20:00:41 -0600

    Arreglando navbar
    Tenia como nombre Archivo, ya le puse Registros clinicos como dicta el figma

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -23,7 +23,7 @@
 
             <li class="nav-item active dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Archivo
+                REGISTROS CLINICOS
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="/propietario">Propietario</a>
