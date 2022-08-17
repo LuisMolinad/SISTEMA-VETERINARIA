7d3c8209d530596c4de18c147cacd5bd1e976e5b
Luis Molina <alfonsomduque@gmail.com>
2022-08-17 09:24:08 -0600

    correcion validacion C cita vacuna

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="shortcut icon" href="{{asset('images/logo.jpeg')}}">
+    <link rel="shortcut icon" href="{{ asset('images/logo.jpeg') }}">
     <title>@yield('titulo')</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.js" integrity="sha512-uplugzeh2/XrRr7RgSloGLHjFV0b4FqUtbT5t9Sa/XcilDr1M3+88u/c+mw6+HepH7M2C5EVmahySsyilVHI/A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.js"
+        integrity="sha512-uplugzeh2/XrRr7RgSloGLHjFV0b4FqUtbT5t9Sa/XcilDr1M3+88u/c+mw6+HepH7M2C5EVmahySsyilVHI/A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/locales-all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.css">
 
     <!--Creo este script para poder englobar las url-->
@@ -23,11 +27,11 @@
         var baseURL = {!! json_encode(url('/')) !!}
     </script>
 
-     <!--Scrips-->
-    <script src="{{asset('js/agenda.js')}}" defer></script>
+    <!--Scrips-->
+    <script src="{{ asset('js/agenda.js') }}" defer></script>
 
     <!--CSS Local-->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!--JS Local-->
 
@@ -43,23 +47,27 @@
     <!--Bootstrap-->
     <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     -->
 </head>
+
 <body>
     @include('navbar')
     <header>
-    @yield('header')
+        @yield('header')
     </header>
 
     <main>
-    @yield('content')
+        @yield('content')
     </main>
 </body>
 
-    @yield('js')
-    <!--Date picker-->
-    <!--
+@yield('js')
+<!--Date picker-->
+<!--
         <input class="date form-control" type="text" readonly="readonly">
     -->
+
 </html>
--- resources/views/layouts/crear_recordatorio.blade.php
+++ resources/views/layouts/crear_recordatorio.blade.php
@@ -1,18 +1,21 @@
 <br>
-<center><h5>Recordatorio</h5></center>
+<center>
+    <h5>Recordatorio</h5>
+</center>
 <br>
 <section class="recordatorio_crear_seccion">
     <div class="form-row">
         <div class="form-group col-md-6">
-            <strong>   <label for="ConceptoCirugia" style="color:black">Anticipacion:</label></strong>
-            <select name="dias_de_anticipacion" class="form-control" id="dias_de_anticipacion" onchange="actualizar_mensaje_al_crear();" 
-            onclick="funcionesOnClick();">
+            <strong> <label for="ConceptoCirugia" style="color:black">Anticipacion:</label></strong>
+            <select name="dias_de_anticipacion" class="form-control" id="dias_de_anticipacion"
+                onchange="actualizar_mensaje_al_crear();" onclick="funcionesOnClick();">
                 <option value="0" selected>No, no deseo un recordatorio</option>
                 <option value="1">1 dias de anticipacion</option>
                 <option value="2">2 dias de anticipacion</option>
                 <option value="3">3 dias de anticipacion</option>
             </select>
-            <div class="invalid-feedback"> <!--Validacion -->
+            <div class="invalid-feedback">
+                <!--Validacion -->
                 Por favor ingrese información sobre el concepto de cirugía
             </div>
             <div class="valid-feedback">
@@ -21,19 +24,18 @@
         </div>
 
         <div class="form-group col-md-6">
-            <strong>   <label style="color:black">Pre visualizacion del mensaje</label></strong>
+            <strong> <label style="color:black">Pre visualizacion del mensaje</label></strong>
             <div id="mensaje-recordatorio">
-            <div id="mensaje-recordatorio-limpieza">
+                <div id="mensaje-recordatorio-limpieza">
+                </div>
             </div>
         </div>
-    </div>
 </section>
 
 @section('js')
     <script>
-        function funcionesOnClick(){
-            actualizar_mensaje_al_crear_vacuna(); 
-    }
-
+        function funcionesOnClick() {
+            actualizar_mensaje_al_crear_vacuna();
+        }
     </script>
 @endsection
