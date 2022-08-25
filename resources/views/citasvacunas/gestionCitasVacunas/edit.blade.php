f59d90e8544d33f5480a8d021ff8a64557bae1ed
Luis Molina <alfonsomduque@gmail.com>
2022-08-25 15:14:29 -0600

    falta funcion

--- resources/views/citasvacunas/gestionCitasVacunas/edit.blade.php
+++ resources/views/citasvacunas/gestionCitasVacunas/edit.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{ asset('css/recordatorio.css') }}">
     <!--Fin del CSS para recordatorios -->
     <!--JS para recordatorios -->
-    <script src=" {{ asset('js/recordatorio.js') }} "></script>
+    <script src="{{ asset('js/recordatorio.js') }}"></script>
     <!--Fin del JS para recordatorios -->
     <!-- Cualquier duda o comentario comunicarse con Rosalio -->
 @endsection
@@ -125,6 +125,37 @@
                 <input type="text" class="form-control" name="filtervacunas" id="filtervacunas"
                     aria-describedby="filtervacunas" value="citasVacunacion">
             </div>
+
+            <section class="recordatorio_crear_seccion">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <strong> <label for="ConceptoCirugia" style="color:black">Anticipacion:</label></strong>
+                        <select name="dias_de_anticipacion" class="form-control" id="dias_de_anticipacion"
+                            onchange="actualizar_mensaje_al_crear_vacuna();"
+                            onclick="actualizar_mensaje_al_crear_vacuna();">
+                            <option value="0" selected>No, no deseo un recordatorio</option>
+                            <option value="1">1 dias de anticipacion</option>
+                            <option value="2">2 dias de anticipacion</option>
+                            <option value="3">3 dias de anticipacion</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            <!--Validacion -->
+                            Por favor ingrese información sobre el concepto de cirugía
+                        </div>
+                        <div class="valid-feedback">
+                            Dato válido sobre concepto de cirugía
+                        </div> <!-- Fin de la validacion -->
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <strong> <label style="color:black">Pre visualizacion del mensaje</label></strong>
+                        <div id="mensaje-recordatorio">
+                            <div id="mensaje-recordatorio-limpieza">
+                            </div>
+                        </div>
+                    </div>
+            </section>
+
             <button type="submit" style=" width: 100px; height: 50px;" class="btn btn-primary">Guardar</button>
 
         </form>
@@ -152,5 +183,9 @@
                     }, false)
                 })
         })()
+
+        function funcionesOnClick() {
+            actualizar_mensaje_al_crear_vacuna();
+        }
     </script>
 @endsection
