1be8deb97907915ce1156ee0ee87b5069ac09e92
Rosalio Monterrosa <rosalioalfre2@gmail.com>
2022-08-27 20:52:59 -0600

    Validaciones

--- resources/views/recordatorio/enviar_mensaje_ui.blade.php
+++ resources/views/recordatorio/enviar_mensaje_ui.blade.php
@@ -64,3 +64,28 @@ Enviar mensaje
     </form>
 </div>
 @endsection
+
+@section('js')
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+    </script>
+@endsection
