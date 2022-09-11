29afa7e2ce17b2da99b7050c38cc92d414a679cf
Luis Molina <alfonsomduque@gmail.com>
2022-09-11 14:12:28 -0600

    middleware en rutas que no tenían

--- app/Http/Controllers/CitaCirugiaController.php
+++ app/Http/Controllers/CitaCirugiaController.php
@@ -21,17 +21,16 @@ class CitaCirugiaController extends Controller
     public function index()
     {
         $mascotas = mascota::with('propietario')->get();
-        return view(('Cirugia.GestionarCirugia'),compact('mascotas'));
-
+        return view(('Cirugia.GestionarCirugia'), compact('mascotas'));
     }
 
     /*Para el PDF */
     public function pdf($id)
     {
         $mascotas = mascota::FindOrFail($id);
-        $pdf = PDF::loadView('Cirugia.pdf' ,['mascotas'=>$mascotas]);
+        $pdf = PDF::loadView('Cirugia.pdf', ['mascotas' => $mascotas]);
         return $pdf->stream();
-     }
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -56,19 +55,19 @@ class CitaCirugiaController extends Controller
     public function store(Request $request)
     {
         //Recordatorio
-        if(request('dias_de_anticipacion') != 0){
-            $datosRecordatorio = 
-            [
-                'id' => recordatorio::max('id') + 1,
-                'estado' => 0, //*0 = no enviado, -1 fallo al enviar, 1 envio exitoso
-                'dias_de_anticipacion' => request('dias_de_anticipacion'),
-                'fecha' => request('start'),
-                'concepto' => request('conceptoCirugia'),
-                'nombre' => request('nombre_mascota'),
-                'id_mascota' => request('title'),
-                'telefono' => request('telefono'),
-                'id_propietario'=>request('id_propietario')
-            ];
+        if (request('dias_de_anticipacion') != 0) {
+            $datosRecordatorio =
+                [
+                    'id' => recordatorio::max('id') + 1,
+                    'estado' => 0, //*0 = no enviado, -1 fallo al enviar, 1 envio exitoso
+                    'dias_de_anticipacion' => request('dias_de_anticipacion'),
+                    'fecha' => request('start'),
+                    'concepto' => request('conceptoCirugia'),
+                    'nombre' => request('nombre_mascota'),
+                    'id_mascota' => request('title'),
+                    'telefono' => request('telefono'),
+                    'id_propietario' => request('id_propietario')
+                ];
 
             $datoscirugia = [
                 'recordatorio_id' => recordatorio::max('id') + 1,
@@ -77,15 +76,14 @@ class CitaCirugiaController extends Controller
                 'conceptoCirugia' => request('conceptoCirugia'),
                 'recomendacionPreoOperatoria' => request('recomendacionPreoOperatoria'),
                 'end' => request('end'),
-                'groupId'=>request('groupId'),
-                'filtercirugias' =>request('filtercirugias'),
+                'groupId' => request('groupId'),
+                'filtercirugias' => request('filtercirugias'),
                 'title' => request('title')
             ];
 
             recordatorio::insert($datosRecordatorio);
-            citaCirugia ::insert($datoscirugia);
-        }
-        else if(request('dias_de_anticipacion') == 0){
+            citaCirugia::insert($datoscirugia);
+        } else if (request('dias_de_anticipacion') == 0) {
             $datoscirugia = [
                 'recordatorio_id' => null,
                 'start' => request('start'),
@@ -93,12 +91,12 @@ class CitaCirugiaController extends Controller
                 'conceptoCirugia' => request('conceptoCirugia'),
                 'recomendacionPreoOperatoria' => request('recomendacionPreoOperatoria'),
                 'end' => request('end'),
-                'groupId'=>request('groupId'),
-                'filtercirugias' =>request('filtercirugias'),
+                'groupId' => request('groupId'),
+                'filtercirugias' => request('filtercirugias'),
                 'title' => request('title')
             ];
 
-            citaCirugia ::insert($datoscirugia);
+            citaCirugia::insert($datoscirugia);
         }
         //Finaliza recordatorio
 
@@ -114,24 +112,21 @@ class CitaCirugiaController extends Controller
      */
     public function show($id)
     {
-        
-      $cita = citaCirugia::FindOrFail($id);
+
+        $cita = citaCirugia::FindOrFail($id);
         $mascota = mascota::where('id', $cita->mascota_id)->with('propietario')->get();
 
-       $propietario = propietario::where('id', $mascota[0]->propietario_id)->get();
-       $recordatorio = recordatorio::where('id', $cita->recordatorio_id)->first();
+        $propietario = propietario::where('id', $mascota[0]->propietario_id)->get();
+        $recordatorio = recordatorio::where('id', $cita->recordatorio_id)->first();
 
-         $datos = [
-             'mascotas' => $mascota,
-             'citaCirugias' => citaCirugia::where('id',$id)->get(),
-             'propietarios' => $propietario,
-             'recordatorios' => $recordatorio
-         ];
+        $datos = [
+            'mascotas' => $mascota,
+            'citaCirugias' => citaCirugia::where('id', $id)->get(),
+            'propietarios' => $propietario,
+            'recordatorios' => $recordatorio
+        ];
 
-       return view('Cirugia.gestionar_cirugias.show', compact('datos'));
-       
-   
-
+        return view('Cirugia.gestionar_cirugias.show', compact('datos'));
     }
 
     /**
@@ -145,15 +140,16 @@ class CitaCirugiaController extends Controller
     {
         //Obtengo la informacion al darle click a un evento por medio de su id
         $citaCirugia = citaCirugia::find($id);
-        $citaCirugia->start=Carbon::createFromFormat('Y-m-d H:i:s', $citaCirugia->start)->format('Y-m-d');
+        $citaCirugia->start = Carbon::createFromFormat('Y-m-d H:i:s', $citaCirugia->start)->format('Y-m-d');
         return response()->json($citaCirugia);
     }
 
-    public function mostrar($id){
-    
-        $mascotas=mascota::with('propietario')->findOrFail($id);
-       
-        return view('Cirugia.CrearCirugia',compact('mascotas'));
+    public function mostrar($id)
+    {
+
+        $mascotas = mascota::with('propietario')->findOrFail($id);
+
+        return view('Cirugia.CrearCirugia', compact('mascotas'));
     }
 
     /**
@@ -166,43 +162,40 @@ class CitaCirugiaController extends Controller
     public function update(Request $request, $id)
     {
 
-        $datosCitaCirugia = 
-        [
-            'id' => $id,
-            'conceptoCirugia' => request('conceptoCirugia'),
-            'start' => request('start'),
-            'recomendacionPreoOperatoria' => request('recomendacionPreoOperatoria'),
-        ];
+        $datosCitaCirugia =
+            [
+                'id' => $id,
+                'conceptoCirugia' => request('conceptoCirugia'),
+                'start' => request('start'),
+                'recomendacionPreoOperatoria' => request('recomendacionPreoOperatoria'),
+            ];
         citaCirugia::where('id', $id)->update($datosCitaCirugia);
 
 
-        $mascota_id=citaCirugia::where('id',$id)->get('mascota_id')->first();
+        $mascota_id = citaCirugia::where('id', $id)->get('mascota_id')->first();
         // $datos =  [
         //     $mascota_id = request('id'),
         //     $datos = citaCirugia::all()->where('mascota_id', $mascota_id),
         //     $mascotas = mascota::find($id),
         // ];
-       // return redirect('GestionCirugia.update');
-       //Me falta que me mande a la vista
+        // return redirect('GestionCirugia.update');
+        //Me falta que me mande a la vista
 
-       try{
-        $datosRecordatorio=[
-            'concepto' => request('conceptoCirugia'),
-            'fecha' => request('start'),
-            'dias_de_anticipacion'=>request('dias_de_anticipacion')
-        ];
-    
-           $recordatorio_id = citaCirugia::where('id', $id)->first();
-    
-           recordatorio::where('id', $recordatorio_id->recordatorio_id )->update($datosRecordatorio);
-       }
-       catch(Exception $exception){
-        $exception->getMessage();
-       }
-       
-       return redirect('citacirugia/index/gestionarCirugia/'.$mascota_id->mascota_id)->with('warning', 'Cita de cirugía ha sido editada correctamente');
-      
+        try {
+            $datosRecordatorio = [
+                'concepto' => request('conceptoCirugia'),
+                'fecha' => request('start'),
+                'dias_de_anticipacion' => request('dias_de_anticipacion')
+            ];
 
+            $recordatorio_id = citaCirugia::where('id', $id)->first();
+
+            recordatorio::where('id', $recordatorio_id->recordatorio_id)->update($datosRecordatorio);
+        } catch (Exception $exception) {
+            $exception->getMessage();
+        }
+
+        return redirect('citacirugia/index/gestionarCirugia/' . $mascota_id->mascota_id)->with('warning', 'Cita de cirugía ha sido editada correctamente');
     }
 
     /**
@@ -215,28 +208,25 @@ class CitaCirugiaController extends Controller
     {
 
         //Recordatorio destroy
-        try{
-            $recordatorio_id = citaCirugia::where('id',$id)->first();
+        try {
+            $recordatorio_id = citaCirugia::where('id', $id)->first();
             recordatorio::destroy($recordatorio_id->recordatorio_id);
-        }
-        catch(Exception $e){
-
+        } catch (Exception $e) {
         }
 
-        $registro = citaCirugia::where('id',$id)->get('mascota_id');
+        $registro = citaCirugia::where('id', $id)->get('mascota_id');
 
         citaCirugia::destroy($id);
 
-        return redirect('/citacirugia/index/gestionarCirugia/'. $registro[0]->mascota_id)->with('danger', 'Cita de cirugía eliminada correctamente');
+        return redirect('/citacirugia/index/gestionarCirugia/' . $registro[0]->mascota_id)->with('danger', 'Cita de cirugía eliminada correctamente');
     }
 
-    public function gestionar_cirugias_por_mascota($id){
+    public function gestionar_cirugias_por_mascota($id)
+    {
         $mascota_id = request('id');
         $datos = citaCirugia::all()->where('mascota_id', $mascota_id);
         $mascotas = mascota::find($id);
-        return view('Cirugia.gestionar_cirugias.index', compact('mascotas','datos'));
-
-
+        return view('Cirugia.gestionar_cirugias.index', compact('mascotas', 'datos'));
     }
 
     //Se utiliza para mostrar las citas en el calendario
@@ -248,7 +238,7 @@ class CitaCirugiaController extends Controller
     }
 
     //Se utiliza para actualizar cita
-     public function editarCirugia($id)
+    public function editarCirugia($id)
     {
         $cita = citaCirugia::FindOrFail($id);
         $mascota = mascota::where('id', $cita->mascota_id)->with('propietario')->get();
@@ -257,12 +247,11 @@ class CitaCirugiaController extends Controller
 
         $datos = [
             'mascotas' => $mascota,
-            'citaCirugias' => citaCirugia::where('id',$id)->get(),
+            'citaCirugias' => citaCirugia::where('id', $id)->get(),
             'propietarios' => $propietario,
             'recordatorio' => $recordatorio
         ];
-        
-       return view('Cirugia.gestionar_cirugias.edit', compact('datos','cita'));
 
+        return view('Cirugia.gestionar_cirugias.edit', compact('datos', 'cita'));
     }
 }
