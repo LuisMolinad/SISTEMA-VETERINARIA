b31d2c18f455862101167ef284d372ab949f8a5b
Luis Molina <alfonsomduque@gmail.com>
2022-09-12 09:58:52 -0600

    cambio vacuna reinicio mensaje recordatorio

--- resources/views/citasvacunas/create.blade.php
+++ resources/views/citasvacunas/create.blade.php
@@ -168,9 +168,8 @@
                     <div class="form-group col-md-6">
                         <strong> <label for="ConceptoCirugia" style="color:black">Anticipacion:</label></strong>
                         <select name="dias_de_anticipacion" class="form-control" id="dias_de_anticipacion"
-                            onchange="actualizar_mensaje_al_crear_vacuna();"
                             onclick="actualizar_mensaje_al_crear_vacuna();">
-                            <option value="0" selected>No, no deseo un recordatorio</option>
+                            <option value="0">No, no deseo un recordatorio</option>
                             <option value="1">1 dias de anticipacion</option>
                             <option value="2">2 dias de anticipacion</option>
                             <option value="3">3 dias de anticipacion</option>
