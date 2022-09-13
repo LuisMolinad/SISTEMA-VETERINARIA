5f46d95f99739348c9b231dc43d4a37d7aed02a5
Luis Molina <alfonsomduque@gmail.com>
2022-09-13 14:05:07 -0600

    roles-permisos asignados en seeder, faltante delimitar funciones

--- database/seeders/RolesAndPermissionSeeder.php
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\propietario;
 use Illuminate\Database\Seeder;
 //
 use Spatie\Permission\Models\Role;
@@ -16,20 +17,119 @@ class RolesAndPermissionSeeder extends Seeder
      */
     public function run()
     {
-        //aca tengo que crear los roles y los permisos
+        // TODO aca tengo que crear los roles y los permisos
 
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // create permissions
-
+        // Roles
         Permission::create(['name' => 'ver-rol']);
         Permission::create(['name' => 'editar-rol']);
         Permission::create(['name' => 'crear-rol']);
         Permission::create(['name' => 'borrar-rol']);
+
+
+        // TODO Propietario
+        $propietario = [
+            Permission::create(['name' => 'ver-Propietario']),
+            Permission::create(['name' => 'editar-Propietario']),
+            Permission::create(['name' => 'crear-Propietario']),
+            Permission::create(['name' => 'borrar-Propietario']),
+        ];
+
+        // TODO Mascota
+        $Mascota = [
+            Permission::create(['name' => 'ver-Mascota']),
+            Permission::create(['name' => 'editar-Mascota']),
+            Permission::create(['name' => 'crear-Mascota']),
+            Permission::create(['name' => 'borrar-Mascota']),
+        ];
+
+        // TODO Cirugia
+        $cirugia = [
+            Permission::create(['name' => 'ver-Cirugia']),
+            Permission::create(['name' => 'editar-Cirugia']),
+            Permission::create(['name' => 'crear-Cirugia']),
+            Permission::create(['name' => 'borrar-Cirugia']),
+        ];
+
+        //TODO Actas Defuncion
+        $actasDefuncion = [
+            Permission::create(['name' => 'ver-ActasDefuncion']),
+            Permission::create(['name' => 'crear-ActasDefuncion']),
+        ];
+        // TODO RecetaMedica  
+        $recetaMedica = [
+            Permission::create(['name' => 'ver-RecetaMedica']),
+            Permission::create(['name' => 'crear-RecetaMedica']),
+
+        ];
+
+
+        //TODO Vacuna  
+        $vacuna = [
+            Permission::create(['name' => 'ver-Vacuna']),
+            Permission::create(['name' => 'editar-Vacuna']),
+            Permission::create(['name' => 'crear-Vacuna']),
+            Permission::create(['name' => 'borrar-Vacuna']),
+        ];
+        //TODO CitaVacuna  
+        $citaVacuna = [
+            Permission::create(['name' => 'ver-CitaVacuna']),
+            Permission::create(['name' => 'editar-CitaVacuna']),
+            Permission::create(['name' => 'crear-CitaVacuna']),
+            Permission::create(['name' => 'borrar-CitaVacuna']),
+        ];
+        //TODO Limpieza Dental
+        $limpiezaDental = [
+            Permission::create(['name' => 'ver-LimpiezaDental']),
+            Permission::create(['name' => 'editar-LimpiezaDental']),
+            Permission::create(['name' => 'crear-LimpiezaDental']),
+            Permission::create(['name' => 'borrar-LimpiezaDental']),
+        ];
+        //TODO CITA SERVICIO 
+        $citaServicio = [
+            Permission::create(['name' => 'ver-CitaServicio']),
+            Permission::create(['name' => 'editar-CitaServicio']),
+            Permission::create(['name' => 'crear-CitaServicio']),
+            Permission::create(['name' => 'borrar-CitaServicio']),
+        ];
+
+        //TODO Recordatorio
+        $recordatorio = [
+            Permission::create(['name' => 'ver-Recordatorio']),
+            Permission::create(['name' => 'editar-Recordatorio']),
+            Permission::create(['name' => 'crear-Recordatorio']),
+            Permission::create(['name' => 'borrar-Recordatorio']),
+        ];
+
+
+        //TODO TIPO SERVICIO
+        $tipoServicio = [
+            Permission::create(['name' => 'ver-TipoServicio']),
+            Permission::create(['name' => 'editar-TipoServicio']),
+            Permission::create(['name' => 'crear-TipoServicio']),
+            Permission::create(['name' => 'borrar-TipoServicio']),
+        ];
+
         // create roles and assign created permissions
         //$role = Role::create(['name' => 'Asistente'])->givePermissionTo(Permission::all());
-        $role = Role::create(['name' => 'Asistente']);
+        $roleDoctora = Role::create(['name' => 'Doctora'])->givePermissionTo([
+            $propietario, $Mascota, $cirugia,
+            $actasDefuncion, $recetaMedica,  $vacuna
+
+        ]);
+        $roleAsistente = Role::create(['name' => 'Asistente Administrativo'])->givePermissionTo([
+            $propietario, $Mascota, $citaVacuna,
+            $vacuna, $cirugia, $limpiezaDental,
+            $citaServicio, $recordatorio
+        ]);
+        $roleGerente = Role::create(['name' => 'Gerente General'])->givePermissionTo([
+            $citaVacuna, $limpiezaDental,
+            $cirugia, $tipoServicio,
+            $citaServicio, $recordatorio
+        ]);
+        //$role = Role::create(['name' => 'Asistente']);
         //$role->givePermissionTo('edit articles');
 
         // or may be done by chaining
