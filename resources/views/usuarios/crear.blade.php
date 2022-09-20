001dc5c8a04032c2ac5c08e84cf50bd4e3d482c9
Luis Molina <alfonsomduque@gmail.com>
2022-09-20 10:00:23 -0600

    Mejoras crear usuario, editar recupera el rol

--- resources/views/usuarios/crear.blade.php
+++ resources/views/usuarios/crear.blade.php
@@ -17,7 +17,7 @@
 
     {{-- Manejador de Errores que muestra aquellos que no estan llenos --}}
     @if ($errors->any())
-        <div class="alert alert-dark alert-dismissible fade show"role="alert">
+        <div class="alert alert-dark alert-dismissible fade show" role="alert">
             <strong>¡Revise los campos!</strong>
             @foreach ($errors->all() as $error)
                 <span class="badge badge-danger">{{ $error }}</span>
@@ -30,37 +30,50 @@
     {{-- El formulario siguiente se ha hecho con el uso de la libreria HTML COLLECTIVE
     Su funcion principal es facilitar la creacion de formularios de envio de alguna variable
     Para mas informacion revisar la docunentacion oficial --}}
-    {!! Form::open(['route' => 'usuarios.store', 'method' => 'POST']) !!}
-    <div class="container">
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="name">Nombre</label>
-                {{-- Las variables deben ir exactamente como se reciben en el controlador --}}
-                {!! Form::text('name', null, ['class' => 'form-control']) !!}
-            </div>
-            <div class="form-group col-md-6">
-                <label for="email">E-mail</label>
-                {!! Form::text('email', null, ['class' => 'form-control']) !!}
-            </div>
-            <div class="form-group col-md-6">
-                <label for="password">Password</label>
-                {!! Form::password('password', ['class' => 'form-control']) !!}
-            </div>
-            <div class="form-group col-md-6">
-                <label for="confirm-password">Confirmar Password</label>
-                {!! Form::password('confirm-password', ['class' => 'form-control']) !!}
-            </div>
-            <div class="form-group col-md-6">
-                <label for="">Roles</label>
-                {!! Form::select('roles[]', $roles, [], ['class' => 'form-control']) !!}
-            </div>
+    {{-- {!! Form::open(['route' => 'usuarios.store', 'method' => 'POST']) !!} {{route('GestionCirugia.update',$cita->id)}} --}}
+    <form action="{{ route('usuarios.store') }}" method="post">
+        @csrf
+        @method('POST')
+        <div class="container">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="name">Nombre</label>
+                    {{-- Las variables deben ir exactamente como se reciben en el controlador --}}
+                    {{-- {!! Form::text('name', null, ['class' => 'form-control']) !!} --}}
+                    <input type="text" class="form-control" id="name" name="name"
+                        value=" {{ old('name') }} "required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="email">E-mail</label>
+                    {{-- {!! Form::text('email', null, ['class' => 'form-control']) !!} --}}
+                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}"
+                        required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="password">Password</label>
+                    {{-- {!! Form::password('password', ['class' => 'form-control']) !!} --}}
+                    <input type="password" class="form-control" id="password" name="password"
+                        value=" {{ old('password') }} " required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="confirm-password">Confirmar Password</label>
+                    {{-- {!! Form::password('confirm-password', ['class' => 'form-control']) !!} --}}
+                    <input type="password" class="form-control" id="confirm-password" name="confirm-password"
+                        value="{{ old('confirm-password') }}" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="">Roles</label>
+                    {!! Form::select('roles[]', $roles, [], ['class' => 'form-control']) !!}
 
+                </div>
+
+            </div>
+            @can('crear-Usuarios')
+                <button type="submit"class="btn btn-primary">Guardar</button>
+            @endcan
         </div>
-        @can('crear-Usuarios')
-            <button type="submit"class="btn btn-primary">Guardar</button>
-        @endcan
-    </div>
-    {!! Form::close() !!}
+    </form>
+    {{-- {!! Form::close() !!} --}}
 
 
 
