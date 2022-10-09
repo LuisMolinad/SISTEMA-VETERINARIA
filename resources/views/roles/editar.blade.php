db270e4a28b4a52a1dce663969e0549b869ba2cf
Luis Molina <alfonsomduque@gmail.com>
2022-10-09 13:00:23 -0600

    tabla en editar permiso

--- resources/views/roles/editar.blade.php
+++ resources/views/roles/editar.blade.php
@@ -7,6 +7,11 @@
 @section('librerias')
     <!--Data tables-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    {{-- Tablas --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.0/dist/bootstrap-table.min.css">
 @endsection
 
 @section('header')
@@ -26,41 +31,46 @@
             </button>
         </div>
     @endif
-    {{-- El formulario siguiente se ha hecho con el uso de la libreria HTML COLLECTIVE
-   Su funcion principal es facilitar la creacion de formularios de envio de alguna variable
-   Para mas informacion revisar la docunentacion oficial --}}
-    {{-- {!! Form::model($role, ['method' => 'PUT', 'route' => ['roles.update', $role->id]]) !!} --}}
+    {{--  --}}
+
     <form action="{{ route('roles.update', $role->id) }}" method="POST">
         {{-- @csrf --}}
         @csrf
         {{ method_field('PATCH') }}
         <div class="container">
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="name">Nombre del Rol</label>
-                    {{-- Las variables deben ir exactamente como se reciben en el controlador --}}
-                    {{-- {!! Form::text('name', null, ['class' => 'form-control']) !!} --}}
-                    <input class="form-control" type="text" name="name" id="name" value="{{ $role->name }}">
-                </div>
-                <div class="form-col-md-6">
-                    <label for="email">Permisos para este Rol:</label>
-                    <br>
-                    @foreach ($permission as $value)
-                        <label>
-                            {{-- Compara el array y lo marca como checked si se encuentra en el --}}
-                            <input type="checkbox" name="permission[]" class="name" value="{{ $value->id }} "
-                                @if (in_array($value->id, $rolePermissions) == true) checked @endif>
-                            <label for="">{{ $value->name }}</label>
-                        </label>
-                    @endforeach
-                </div>
+
+            <div class="form-group col-md-6">
+                <label for="name">Nombre del Rol</label>
+                {{-- Las variables deben ir exactamente como se reciben en el controlador --}}
+                {{-- {!! Form::text('name', null, ['class' => 'form-control']) !!} --}}
+                <input class="form-control" type="text" name="name" id="name" value="{{ $role->name }}">
             </div>
+            <div class="table-responsive">
+                <label for="table">Permisos para este Rol:</label>
+                <table class="table w-auto">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Permisos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($permission as $value)
+                            <tr>
+                                <td> <input type="checkbox" name="permission[]" class="name" value="{{ $value->id }} "
+                                        @if (in_array($value->id, $rolePermissions) == true) checked @endif></td>
+                                <td> {{ $value->name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+
 
             <button type="submit"class="btn btn-primary">Guardar</button>
         </div>
     </form>
-    {{-- {!! Form::close() !!} --}}
-
 @endsection
 
 
