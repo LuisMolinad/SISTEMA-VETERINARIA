3e3b7f30d9b6487b1f87bbfd73ce5830740adf32
Luis Molina <alfonsomduque@gmail.com>
2022-10-17 15:41:08 -0600

    frontend graficos

--- resources/views/reporte/index.blade.php
+++ resources/views/reporte/index.blade.php
@@ -4,9 +4,10 @@
     Reportes
 @endsection
 
-@section('librerias')
-    <!--Date picker-->
-@endsection
+
+<link rel="stylesheet" href=" {{ asset('css/grafico.css') }} ">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
+
 
 
 @section('header')
@@ -14,7 +15,156 @@
 @endsection
 
 @section('content')
+    {{-- <div class="graficos-container">
+        <div class="row">
+            <div>
+                <canvas id="myChart" class="column" width="750px">
+                </canvas>
+            </div>
+            <div>
+                <canvas id="myChart1" class="column" width="750px">
+                </canvas>
+            </div>
+            <div>
+                <canvas id="myChart2" class="column" width="750px">
+                </canvas>
+            </div>
+        </div>
+    </div> --}}
+
+    <div class="graficos-container">
+        <div>
+            <canvas id="myChart">
+            </canvas>
+        </div>
+        <div>
+            <canvas id="myChart1">
+            </canvas>
+        </div>
+        <div>
+            <canvas id="myChart2">
+            </canvas>
+        </div>
+    </div>
+
+    {{--  <div class="container">
+
+    </div> --}}
 @endsection
 
 @section('js')
+    <script>
+        // Any of the following formats may be used
+        const ctx = document.getElementById('myChart');
+
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        // Any of the following formats may be used
+        const ctx1 = document.getElementById('myChart1');
+
+        const myChart1 = new Chart(ctx1, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        // Any of the following formats may be used
+        const ctx2 = document.getElementById('myChart2');
+
+        const myChart2 = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 @endsection
